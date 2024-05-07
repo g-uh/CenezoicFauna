17c9036c30325e6e2859fede93170fe9b66b5896
g-uh <jjmaidment@outlook.com>
2024-05-07 18:39:54 +0100

    buh

---
+++ src/main/java/net/guh/cenezoicfauna/CenezoicFaunaClient.java
@@ -0,0 +1,10 @@
+package net.guh.cenezoicfauna;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CenezoicFaunaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/guh/cenezoicfauna/CenezoicFaunaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.guh.cenezoicfauna;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CenezoicFaunaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
